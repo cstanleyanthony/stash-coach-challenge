5490f500f0a556f8a535aad30e507556aa929269
Christian Sarcona <csarcona8@gmail.com>
2020-12-03 13:52:49 -0500

    Initial project setup.

---
+++ StashCoach/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StashCoach
+//
+//  Created by Christian Sarcona on 12/3/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
