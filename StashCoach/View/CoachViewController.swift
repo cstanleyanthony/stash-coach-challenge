a4b745f61bdacf9fb14ee5f9e127172532e50f56
Christian Sarcona <csarcona8@gmail.com>
2020-12-04 23:40:57 -0500

    Setup project structure and began TDD.

---
+++ StashCoach/View/CoachViewController.swift
@@ -0,0 +1,6 @@
+// Created by Christian Sarcona on 12/4/20 for Stash
+// Stash Coach challenge
+// 
+	
+
+import Foundation
---
+++ StashCoachTests/CoachInteractorTests.swift
@@ -0,0 +1,31 @@
+// Created by Christian Sarcona on 12/4/20 for Stash
+// Stash Coach challenge
+// 
+	
+import XCTest
+@testable import StashCoach
+
+class CoachInteractorTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testInteractorFetchesAchievements() throws {
+        let interactor: CoachInteractorProtocol = CoachInteractor()
+        
+        
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
