266e7156e617ed1d694e70836153c1bb837ca494
Christian Sarcona <csarcona8@gmail.com>
2020-12-05 23:12:01 -0500

    Created a URLService protocol and enum to build URLs for network requests.

---
+++ StashCoach/Services/URLService/URLService.swift
@@ -0,0 +1,32 @@
+// Created by Christian Sarcona on 12/5/20 for Stash
+// Stash Coach challenge
+// 
+	
+
+import Foundation
+
+/// A URL service enum to build URLs for network requests.
+/// - `baseUrl`:  A default base url to be used with the case based paths
+/// - `url`:  A computed property that gets built from the case of itself
+/// - `url(withBase:path:)`:  A method that can use a different base url and a non case based path,
+/// - `url(withBase:)`:  A method to use a different base url with an existing case based path.
+enum URLService: String, URLServiceProtocol {
+    
+    case achievements = "/achievements"
+    
+    var baseUrl: String {
+        return "https://server.stash.com"
+    }
+    
+    var url: URL? {
+        return URL(string: baseUrl + self.rawValue)
+    }
+    
+    func url(withBase base: String, path: String) -> URL? {
+        return URL(string: base + path)
+    }
+    
+    func url(withBase base: String) -> URL? {
+        return URL(string: base + self.rawValue)
+    }
+}
---
+++ StashCoach/Services/URLService/URLServiceProtocol.swift
@@ -0,0 +1,14 @@
+// Created by Christian Sarcona on 12/5/20 for Stash
+// Stash Coach challenge
+// 
+	
+
+import Foundation
+
+/// A protocol to be used for a URL service.
+/// - Description: Is typically used with an enum where the paths are raw values of the cases.
+protocol URLServiceProtocol {
+    var baseUrl: String { get }
+    var url: URL? { get }
+    func url(withBase base: String, path: String) -> URL?
+}
